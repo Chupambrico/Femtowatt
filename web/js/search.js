0b3c13e7359027e7e2ff1b5d255ce0ff693621b7
Rocca94 <gabriel.roccabruna@gmail.com>
2013-05-30 17:36:20 +0200

    Update search.js

--- web/js/search.js
+++ web/js/search.js
@@ -1,7 +1,7 @@
 var i=0;
 var xmlhttp,nPagine;
 var nomeSpecial,bottoneDisabled;
-var unaSola;
+var unaSola,pagina,errS;
 
 function nPagina(){
     xmlhttp=GetXmlHttpObject();
@@ -64,7 +64,8 @@ function avvio(valore){
     document.getElementById('errore').className = "";
     document.getElementById('errore').innerHTML = "";
     document.getElementById('numeriPagine').innerHTML = "";
-    cerca1(valore,0);
+    if(errS==0){
+    cerca1(valore,0);}
 }
 
 function cerca1(valore,disabilitare){
@@ -95,10 +96,13 @@ function risultato(){
         stringa = stringa.replace(Re,"");
         var n=stringa.split("/");
         $('#search').typeahead({source: n});
+     
+        
     }
 }
 
 function lista(){
+  
     xmlhttp=GetXmlHttpObject();
     if (xmlhttp==null){
         alert ("Your browser does not support Ajax HTTP");
@@ -121,3 +125,44 @@ function GetXmlHttpObject(){
     }
     return null;
 }
+  
+    
+    function anteprimaOut(){
+        if (xmlhttp.readyState==4){
+            var stringa= xmlhttp.responseText.trim();
+            document.getElementById("lista").innerHTML=stringa; 
+            lista();
+        }
+    
+    }
+    function anteprima(){
+        
+        xmlhttp=GetXmlHttpObject();
+        if (xmlhttp==null){
+            alert ("Your browser does not support Ajax HTTP");
+            return;
+        }
+       
+
+        var url="top10.jsp";
+        url=url;
+
+        xmlhttp.onreadystatechange=anteprimaOut;
+        xmlhttp.open("GET",url,true);
+        xmlhttp.send(null);
+    }
+    
+    function search1(){
+        var stringa=document.cerca.cerc.value;
+        var regexp =/^[a-zA-Z0-9\s]+$/;
+        
+        if (regexp.test(stringa) == false){
+            document.getElementById("s1").className="control-group error";
+            errS=1;
+        }else{
+            titolo=stringa;
+            document.getElementById("s1").className="control-group success";
+            errS=0;
+        }
+    }
+ 
